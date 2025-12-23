c4ecdd0a39da181a615ed2a570842c92bc26ac4e
terrythomas00 <terry.thomas00@gmail.com>
2025-12-23 15:04:37 -0500

    Add multiply function

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-#def multiply(a, b):
-    #return a * b
+def multiply(a, b):
+    """Multiplies two numbers and returns the result."""
+    return a * b
 
 if __name__ == "__main__":
     print(add(5, 3))       
